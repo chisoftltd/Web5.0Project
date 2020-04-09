9ff11febea394e128283296e7352b2a8bdae498b
chisoftltd <chisoftltd@gmail.com>
2020-04-09 20:43:45 +0200

    Using JavaScript Data Types

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var $elam = document.getElementById('output');
+$elam.innerHTML += 'Test ';
+$elam.innerHTML += 4 + 6 + ' Demo';
+$elam.innerHTML += ' Demo ' + 4 + 6;
+$elam.innerHTML += ' Demo ' + (4 + 6);
