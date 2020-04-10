e8f1d2a1f05ed4017017f9f6bc1feb35ba3c3c6a
chisoftltd <chisoftltd@gmail.com>
2020-04-10 20:26:18 +0200

    Event Handling

--- js/HandlingCallbacks.js
+++ js/HandlingCallbacks.js
@@ -1,5 +1,5 @@
 function handleClick(callback){
-    alert('This button has been clicked' );
+    alert('This button has been clicked');
     if(callback) {
         callback();
     }
@@ -12,3 +12,21 @@ function doMore(){
 function doSomethingElse(){
     document.writeln('Test Message' + '<br/>' + 'Second Message')
 }
+
+function handleClick2(){
+    document.getElementById("dateTarget2").innerHTML = Date();
+}
+
+document.getElementById('dateButton').onclick = handleClick3;
+function handleClick3(){
+    document.getElementById('dateTarget3').innerHTML = Date();
+}
+
+document.getElementById('dateButton2').addEventListener('click', handleClick4)
+function handleClick4(){
+    document.getElementById('dateTarget4').innerHTML = Date();
+}
+
+document.getElementById('dateButton3').addEventListener('click', function handleClick(){
+    document.getElementById('dateTarget5').innerHTML = Date();
+});
